936fc2ccd2c6d12ad8fc1942ac6959ee77e295ff
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-05-28 00:22:04 -0500

    made the listItems array and double checked to see if working

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -9,3 +9,10 @@ function addItem(item){
     basket.push(item)
     return true
 }
+
+function listItems(){
+    console.log('Items in basket are:');
+for (let i = 0; i<basket.length; i++) {
+     console.log(basket[i]);
+}
+}
